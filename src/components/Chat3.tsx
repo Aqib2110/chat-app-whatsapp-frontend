6cd8fb8b8c935823674115e1954a32985b43e723
Aqib2110 <muhammadaqib2110@gmail.com>
2025-05-20 12:43:35 +0500

    Update Chat3.tsx

--- src/components/Chat3.tsx
+++ src/components/Chat3.tsx
@@ -2,12 +2,14 @@ import { useState, useEffect, useRef, memo } from 'react';
 const Chat3 = ({ sender, token, contact }: any) => {
   const inputRef = useRef<HTMLInputElement>(null);
   const inputRef1 = useRef<HTMLInputElement>(null);
-  const [messag, setMessag] = useState([]);
+  const [messag, setMessag] = useState([{}]);
+  const [loading, setloading] = useState(true);
 const [fet, setfet] = useState(true)
   useEffect(() => {
     if (!contact._id) return;
    if(fet)
     {
+      setloading(true);
      fetch("https://chat-app-whatsapp-backend.vercel.app/chat", {
       method: "POST",
       headers: {
@@ -20,6 +22,7 @@ const [fet, setfet] = useState(true)
       .then(data => {setMessag(data.messages || [])})
       .catch(console.log);
       setfet(false);
+            setloading(false);
    }    
   }, [contact._id, sender, token,fet]);
     useEffect(() => {
@@ -68,7 +71,6 @@ const [fet, setfet] = useState(true)
     }
   };
 
-
   return (
     <>
       {/* Desktop View */}
@@ -83,7 +85,7 @@ const [fet, setfet] = useState(true)
           ) : (
             
             messag.map((msg: any) => (
-              <div key={msg._id} className='text-white m-2' style={{ display: 'flex', justifyContent: msg.senderId === sender ? "end" : "start" }}>
+              <div key={msg._id + Math.random()} className='text-white m-2' style={{ display: 'flex', justifyContent: msg.senderId === sender ? "end" : "start" }}>
                 {msg.content}
               </div>
             ))
@@ -96,24 +98,32 @@ const [fet, setfet] = useState(true)
       </div>
 
       {/* Mobile View */}
-      <div className=' w-[100vw] md:hidden bg-black h-[100vh]'>
-        <div className='flex justify-between px-1 h-[5vh] items-center border-b text-white'>
+   <div className=' w-[100vw] md:hidden bg-black h-[100vh]'>
+         <div className='flex justify-between w-[100%] fixed px-1 h-[5vh] items-center border-b text-white'>
           <p className=''>{contact.username}</p>
           <h1 className='text-green-500 text-2xl'><a href='/'>WhatsApp</a></h1>
         </div>
-        <div className='h-[90vh]  flex flex-col overflow-auto text-white'>
-          { messag.length === 0 ? (
+         <div className='flex justify-between w-[100%]  px-1 h-[5vh] items-center border-b text-white'>
+        
+        </div>
+        <div className='h-[90vh] border flex flex-col overflow-auto text-white'>
+          
+          { loading ? (
+  <div className="flex items-center justify-center h-full text-gray-300 text-lg">
+    Loading...
+  </div>
+) : messag.length === 0 ? (
             <div className='flex mt-55 self-center'>No Messages</div>
           ) : (
           
             messag.map((msg: any) => (
-              <div key={msg._id} className='text-white m-2' style={{ display: 'flex', justifyContent: msg.senderId === sender ? "end" : "start" }}>
+              <div key={msg._id + Math.random()} className='text-white m-2' style={{ display: 'flex', justifyContent: msg.senderId === sender ? "end" : "start" }}>
                 {msg.content}
               </div>
             ))
           )}
         </div>
-        <div className='flex justify-center items-center  h-[5vh] text-white mb-0 gap-5'>
+        <div className='flex justify-center fixed w-[100%] items-center  h-[5vh] text-white mb-0 gap-5'>
           <input type="text" ref={inputRef1} className='bg-white ml-1 w-[77%] px-2 h-[90%]  text-black' placeholder='Message...' />
           <button onClick={() => sendMessage(inputRef1)} className='bg-white text-black h-[90%] py-1 w-[20%]'>send</button>
         </div>
@@ -123,6 +133,8 @@ const [fet, setfet] = useState(true)
 };
 
 export default memo(Chat3);
+
+
 
 
 
